739bd68ae5196507b41d45a514a97f4ac2121aa6
TaichiOhmi <o6mi5.ta1@gmail.com>
2022-07-03 12:23:41 +0900

    madribs

---
+++ madribs/madlibs.py
@@ -0,0 +1,13 @@
+# fish = 'カンパチ'
+
+# print('私は' + fish + 'が釣りたい')
+# print('私は{}が釣りたい'.format(fish))
+# print(f"私は{fish}が釣りたい")
+
+adj = input('形容詞：　')
+verb1 = input('動詞：　')
+verb2 = input('動詞：　')
+famous_person = input('有名人：　')
+
+madlib = f"プログラミングはとても{adj}! 。毎回とてもワクワクさせてくれるから、私は{verb1}することが大好きだ\
+    脱水症状にならないように{famous_person}のように水分補給して{verb2}しましょう。"
